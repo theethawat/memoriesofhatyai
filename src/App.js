689b3c58db01630d12b301cb3c8853800574ce19
Theethawat Savastham <5910110150@psu.ac.th>
2019-09-07 23:21:26 +0700

    Prepare for Flower

--- src/App.js
+++ src/App.js
@@ -19,16 +19,19 @@ class App extends Component {
   componentDidMount() {
     firebase.initializeApp(FirebaseConfig)
     let database = firebase.database()
-    let congratulation = database.ref("/" + this.state.congratId)
+    let congratulation = database.ref("/congrat/" + this.state.congratId)
     congratulation.on("value", snapshot => {
       let name = (snapshot.val() && snapshot.val().name) || "Annonymous"
       let photo = (snapshot.val() && snapshot.val().photo) || "Annonymous"
       let department =
         (snapshot.val() && snapshot.val().department) || "Annonymous"
+      let congratText =
+        (snapshot.val() && snapshot.val().congratText) || "Annonymous"
       this.setState({
         name: name,
         photo: photo,
-        department: department
+        department: department,
+        congratText: congratText
       })
     })
   }
@@ -38,9 +41,25 @@ class App extends Component {
       <div className="container">
         <br />
         <h2 className="kanit acenter text-uppercase">Congratulation</h2>
-        <h2 className="kanit acenter">ยินดีด้วยนะครับ {this.state.name} </h2>
-        <h5 className="kanitlight acenter">{this.state.department}, Eng PSU</h5>
+        <h2 className="kanit acenter">ยินดีด้วยนะครับ{this.state.name} </h2>
+        <h5 className="kanitlight acenter">{this.state.department} PSU</h5>
         <img class="avatar-pic img-fluid piccenter " src={this.state.photo} />
+        <br />
+        <p className="kanitlight acenter"> {this.state.congratText} </p>
+        <p className="kanit acenter">
+          ติน - A-Intania 50 / CoE'28
+          <br />8 September 2019
+        </p>
+        <br />
+        <p className="kanitlight acenter">
+          <span className="text-info">
+            Memories of Hatyai Project | Graduation Ceremony
+          </span>
+          <br />
+          <b>Faculty of Engineering, Prince of Songkla University </b>
+          <br />
+          Hatyai Songkhla, Thailand
+        </p>
       </div>
     )
   }
