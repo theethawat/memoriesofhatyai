2512281d5ab636cc78c2b666f4fc895e3135b3f9
Theethawat Savastham <5910110150@psu.ac.th>
2019-09-07 22:55:43 +0700

    Initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import "./CongratApp.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import FirebaseConfig from "./Firebase"
+import firebase from "firebase"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      congratId: 0,
+      name: "",
+      photo: "",
+      department: "",
+      congratText: ""
+    }
+  }
+
+  componentDidMount() {
+    firebase.initializeApp(FirebaseConfig)
+    let database = firebase.database()
+    let congratulation = database.ref("/" + this.state.congratId)
+    congratulation.on("value", snapshot => {
+      let name = (snapshot.val() && snapshot.val().name) || "Annonymous"
+      let photo = (snapshot.val() && snapshot.val().photo) || "Annonymous"
+      let department =
+        (snapshot.val() && snapshot.val().department) || "Annonymous"
+      this.setState({
+        name: name,
+        photo: photo,
+        department: department
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <br />
+        <h2 className="kanit acenter text-uppercase">Congratulation</h2>
+        <h2 className="kanit acenter">ยินดีด้วยนะครับ {this.state.name} </h2>
+        <h5 className="kanitlight acenter">{this.state.department}, Eng PSU</h5>
+        <img class="avatar-pic img-fluid piccenter " src={this.state.photo} />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/Firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCtjIqTScn4AVK3Ghb2BL1wlVqje_jWPHw",
+  authDomain: "med44congrat.firebaseapp.com",
+  databaseURL: "https://med44congrat.firebaseio.com",
+  projectId: "med44congrat",
+  storageBucket: "med44congrat.appspot.com",
+  messagingSenderId: "578165062765",
+  appId: "1:578165062765:web:89760e3c2aab061e745364"
+}
+export default firebaseConfig
